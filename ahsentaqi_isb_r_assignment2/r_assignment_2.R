dc381bf6743f54b92cdb05ff3920037b07d81560
ahsentaqikazmi <m.ahsen@inboxbiz.com>
2017-03-25 01:28:54 +0500

    Question # 2 is complete

--- ahsentaqi_isb_r_assignment2/r_assignment_2.R
+++ ahsentaqi_isb_r_assignment2/r_assignment_2.R
@@ -9,3 +9,16 @@ hsptldata
 
 names(hsptldata) <- gsub(".", "", names(hsptldata), fixed = TRUE)
 names(hsptldata)
+
+## Question 2 
+
+#separating weekdays, day of month and year column to get the weedays in separate column
+hsptldata <-  separate( hsptldata, Date , into = c("day_only" , "date_only", "year"), sep = ",")
+
+#counting weekdays to get total visits on a each day
+days_count <- count(hsptldata, day_only)
+
+#getting num of max visits
+max_visits <- max(days_count$n)
+print(days_count)
+print(max_visits)
