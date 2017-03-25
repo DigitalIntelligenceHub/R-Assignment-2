3f241e836eda6b887dc65651fa28571dbdc5297d
obiii <obaidurrehman1994@gmail.com>
2017-03-25 13:51:41 +0500

    Committing Q2

--- Obaid_Islamabad_r_Assignment2/R-Assignment2.r
+++ Obaid_Islamabad_r_Assignment2/R-Assignment2.r
@@ -20,3 +20,18 @@ head(hdf)
 # Q1. Cleaning the column names
 names(hdf)<-gsub("\\.","",names(hdf))
 head(hdf) #dots from column names removed
+
+
+
+# Q2. Which day of the week is expected to have most visits?
+dayPop <-
+  hdf %>%
+  mutate(Day=wday(mdy(Date),label=TRUE)) %>%
+  group_by(Day) %>%
+  summarize(visits=length(Day))
+
+ggplot(dayPop,aes(x=Day,y=visits))+geom_bar(stat="identity",fill="#8E44AD")+ggtitle("Visits by Days")+labs(x="Day",y="Vists")
+
+#The visits on Monday are greater than visits on other days of week, and also the probability of Monday is higher 
+# therefore, Monday is expected to have most visits
+
