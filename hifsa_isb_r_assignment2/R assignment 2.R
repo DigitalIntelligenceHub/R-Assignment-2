9a848557c52678f5d8cdfecaddfa98561d1f9cb8
hifsa-tahreem <hifsa.tahreem@inboxbiz.com>
2017-03-26 12:31:48 +0500

    6. Which Doctor is earning highest?

--- hifsa_isb_r_assignment2/R assignment 2.R
+++ hifsa_isb_r_assignment2/R assignment 2.R
@@ -2,29 +2,36 @@ library(dplyr)
 library(lubridate)
 library(tidyr)
 library(ggplot2)
-"removed the dots in the names"
+"1. removed the dots in the names"
 names(hospitaldata) <- gsub(x = names(hospitaldata),
                         pattern = "\\.",
                         replacement = " ")
 names(hospitaldata)
 
-"day of the week is expected to have most visits"
+"2. day of the week is expected to have most visits"
 df<-hospitaldata$Date
 df<-wday(mdy(df), label=TRUE)
 df
 table(df)
 
-"What is the average age of patients"
+"3. What is the average age of patients"
 nage<-as.numeric(hospitaldata$Age) 
 mean(nage, na.rm = TRUE)
 
-"How many children were entertained"
+"4. How many children were entertained"
 age<-as.numeric(hospitaldata$Age)
 ages<-subset(age, age >= 1 & age < 12)
 length(ages)
 
-"Which gender type had what kind of procedure in abundance? 
+"5. Which gender type had what kind of procedure in abundance? 
 i.e. Female visit mostly because of Gynae Problem"
 select(group_by(hospitaldata, Procedure, Sex) ) %>%
 table() %>%
   View()
+
+"6. Which Doctor is earning highest?"
+a<-hospitaldata$`Amount  Received `
+max(a, na.rm = TRUE, filter =c(hospitaldata$`Amount Received By`) )
+
+"alternatively max(a, na.rm = TRUE, select =c(hospitaldata$`Amount Received By`) ) and 
+hospitaldata$`Amount Received By`[which.max(a)] giving same results "
