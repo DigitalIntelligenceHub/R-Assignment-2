015213c733b67968f21077dd086f0cadbb8f690d
Ammar0347 <k122074@nu.edu.pk>
2017-03-26 13:11:46 +0500

    Question 2
    Converting it to a list of days and obtained the frequency by table()
    function.

--- Ammar_Karachi_Rassignment2.R
+++ Ammar_Karachi_Rassignment2.R
@@ -4,3 +4,16 @@ df <- read.csv("C:/Users/Ammar/Documents/hospitaldata.csv",header=TRUE,sep=",")
 #Question 1. Remove the dots in the names, so it may become easier for you to work through it
 # Assigning column names after substituting dots from it.
 colnames(df) <- gsub("\\.", "", colnames(df))
+
+#Question 2. Which day of the week is expected to have most visits?
+#Table shows that the Monday is the day expected to have most visits.
+strDate <- as.character(df$Date)
+strDate <- strsplit(strDate, ",")
+strWday <- list()
+for(i in 1:length(strDate)){
+  strWday[i] <- strDate[[i]][1]
+}
+strWday <- unlist(strWday, recursive = FALSE)
+maxDay <- table(strWday)
+print(maxDay)
+
