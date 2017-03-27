2d9213736fd7c63c62acfbeb2ace41ad616bd198
umairhanif00 <umairhanif00@gmail.com>
2017-03-27 16:33:13 +0500

    Delete Q18.R

--- Q18.R
+++
@@ -1,9 +0,0 @@
-# Question 18
-dataf$TotalCharges<-as.numeric(as.character(dataf$TotalCharges))
-consult_amount <- dataf%>%
-  select(Procedure,TotalCharges) %>%
-  group_by(Procedure) %>%
-  filter(Procedure == 'X Ray' & Procedure == 'Scalling') %>%
-  summarise(sum(TotalCharges,na.rm=TRUE)) %>%
-  print
-
