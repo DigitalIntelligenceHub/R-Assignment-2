74c52f7e8ca70d9184cebdd0c3f72478b61ad640
Shahid Ammer Yar <Shahid.Ammer@inboxbiz.com>
2017-03-27 19:01:19 +0500

    'q1'

--- Shahid_Ammer_ISB_R_Assesment2_R_Code.R
+++ Shahid_Ammer_ISB_R_Assesment2_R_Code.R
@@ -6,3 +6,9 @@ library(lubridate)
 raw_data <- read.csv("C:/Users/shahid.ammer/Documents/Shahid_Ammer_ISB_R_Assesment2/hospitaldata.csv")
 mydf = tbl_df(raw_data)
 mydf
+
+# Please remove the dots in the names, so it may become easier for you to work through it
+# Question 1
+names(mydf)
+names(mydf) <- sub("[.]+$", "", gsub("[.]+", " ", names(mydf)))
+names(mydf)
