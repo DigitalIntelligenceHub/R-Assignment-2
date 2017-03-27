3e4816d1e587deeb08d83ae4b7b779cb55c2fa10
umairhanif00 <umairhanif00@gmail.com>
2017-03-27 16:26:19 +0500

    Delete Q9.R

--- Q9.R
+++
@@ -1,4 +0,0 @@
-#Question 9
-my_shifts <- my_shifts %>%
-  mutate(Shifts = derivedFactor( "Morning" = (Hour>=6 & Hour<=12),"Afternoon" = (Hour>=12 & Hour<=16),"Evening" = (Hour>=14 & Hour<=19),"Night" =((Hour>=19 & Hour<=23))))
-
